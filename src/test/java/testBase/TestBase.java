4f65d62d65e5afc949c0ff3bfeb8b0ba25296db6
Dcan1256 <dogancanyormaz92@gmail.com>
2023-05-03 15:14:00 -0500

    2nd commit-- utilities are added

--- src/test/java/testBase/TestBase.java
+++ src/test/java/testBase/TestBase.java
@@ -1,5 +1,8 @@
 package testBase;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -8,7 +11,11 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 public class TestBase {
@@ -48,5 +55,19 @@ public class TestBase {
     @AfterMethod
     public void tearDown(){
         driver.quit();
+    }
+
+    public String captureScreen(String tname) throws IOException {
+        String timeStamp= new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+        TakesScreenshot takesScreenshot=(TakesScreenshot)driver;
+        File source = takesScreenshot.getScreenshotAs(OutputType.FILE);
+        String destination= System.getProperty("user.dir")+"\\screenshots\\"+tname+"_"+timeStamp+".png";
+
+        try {
+            FileUtils.copyFile(source, new File(destination));
+        }catch (Exception e){
+            e.getMessage();
+        }
+        return destination;
     }
 }
